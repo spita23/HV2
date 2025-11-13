53309dd505eb2bb6f8cae8e4d5ef79cadcca034b
brynjar <brynjar23@live.com>
2025-11-13 16:19:28 +0000

    spurningar vistaðar í localstorage og nýjar spurningar bætast þar við

--- functions.js
+++ functions.js
@@ -1,21 +1,27 @@
 function loadQuestions() {
-    let questions = JSON.parse(localStorage.getItem('questions')) || [
-        { category: '6', subcategory: 'Tónlist', difficulty: '2', quality: '2', question: 'Hvaða hljómsveit gerði lagið "Rangur maður"', answer: 'Sólstrandargæjarnir' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '2', quality: '', question: 'Hvaða hljómsveit kvað "Á hverjum degi beiglan þurfti bensín, rúblunum fleygði í kerlingu sem brosti"', answer: 'Sniglabandið' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '2', quality: '', question: 'Hver kvað "Smíða skútu, skerpi skauta, bý til þrumuost og grauta, haltu kjafti"', answer: 'Bjartmar Guðlaugsson' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '1', quality: '', question: 'Hvaða íslenska hljómsveit gerði lagið "So Alone"', answer: 'Bang Gang' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '1', quality: '3', question: 'Hvaða hljómsveit gerði lagið "Sehnsucht"', answer: 'Rammstein' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '2', question: 'Hvaða hljómsveit gerði lagið "Trans Europe Express"', answer: 'Kraftwerk' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '2', quality: '2', question: 'Hvaða hljómsveit gerði lagið "Hotel California"', answer: 'Eagles' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Hver sönglaði um "The Black Rider"', answer: 'Tom Waits' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Hvaða hljómsveit gerði lagið "Running Free"', answer: 'Iron Maiden' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Á hvaða diski Radiohead er lagið "Karma Police" að finna', answer: 'Ok Computer' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Á hvaða diski Propellerheads er lagið "Take California" að finna', answer: 'DECKSANDDRUMSANDROCKANDROLL' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '1', quality: '2', question: 'Hvaða hljómsveit gerði lagið "Karma Police"', answer: 'Radiohead' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '1', quality: '', question: 'Hvaða hljómsveit gerði lagið "Hello, I love you"', answer: 'The Doors' },
-        { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Hvaða lag byrjar svona "Didn\'t know what time it was the light were low low low"', answer: 'Starman' }
-    ];
-    return questions;
+    const stored = localStorage.getItem('questions');
+    if (stored) {
+        return JSON.parse(stored);
+    } else {
+        const defaultQuestions = [
+            { category: '6', subcategory: 'Tónlist', difficulty: '2', quality: '2', question: 'Hvaða hljómsveit gerði lagið "Rangur maður"', answer: 'Sólstrandargæjarnir' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '2', quality: '', question: 'Hvaða hljómsveit kvað "Á hverjum degi beiglan þurfti bensín, rúblunum fleygði í kerlingu sem brosti"', answer: 'Sniglabandið' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '2', quality: '', question: 'Hver kvað "Smíða skútu, skerpi skauta, bý til þrumuost og grauta, haltu kjafti"', answer: 'Bjartmar Guðlaugsson' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '1', quality: '', question: 'Hvaða íslenska hljómsveit gerði lagið "So Alone"', answer: 'Bang Gang' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '1', quality: '3', question: 'Hvaða hljómsveit gerði lagið "Sehnsucht"', answer: 'Rammstein' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '2', question: 'Hvaða hljómsveit gerði lagið "Trans Europe Express"', answer: 'Kraftwerk' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '2', quality: '2', question: 'Hvaða hljómsveit gerði lagið "Hotel California"', answer: 'Eagles' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Hver sönglaði um "The Black Rider"', answer: 'Tom Waits' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Hvaða hljómsveit gerði lagið "Running Free"', answer: 'Iron Maiden' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Á hvaða diski Radiohead er lagið "Karma Police" að finna', answer: 'Ok Computer' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Á hvaða diski Propellerheads er lagið "Take California" að finna', answer: 'DECKSANDDRUMSANDROCKANDROLL' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '1', quality: '2', question: 'Hvaða hljómsveit gerði lagið "Karma Police"', answer: 'Radiohead' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '1', quality: '', question: 'Hvaða hljómsveit gerði lagið "Hello, I love you"', answer: 'The Doors' },
+            { category: '6', subcategory: 'Tónlist', difficulty: '3', quality: '', question: 'Hvaða lag byrjar svona "Didn\'t know what time it was the light were low low low"', answer: 'Starman' }
+        ];
+        localStorage.setItem('questions', JSON.stringify(defaultQuestions));
+        return defaultQuestions;
+    }
 }
 
 function getCategory(num) {
